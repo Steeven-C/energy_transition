616dfe5fefbcd5889f1a4c53bf4cc58c7947435a
clement <clementcamara@gmail.com>
2020-12-17 16:21:27 +0100

    insert_data file

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
 import psycopg2
 from code import user, password, host, port, name
 from psycopg2 import OperationalError
+from insert_data import df2
+from sqlalchemy import create_engine
 
 """ creating connection between AWS database and our script"""
+
+
 def create_conection(db_user, db_password, db_host, db_port, db_name):
     try:
         connection = psycopg2.connect(
@@ -12,9 +16,68 @@ def create_conection(db_user, db_password, db_host, db_port, db_name):
             port=db_port,
             database=db_name)
         print("Successful connexion")
+        cursor = connection.cursor()
+        # Executing a SQL query
+        cursor.execute("SELECT version();")
+        # Fetch result
+        record = cursor.fetchone()
+        print("You are connected to - ", record, "\n")
     except OperationalError as error:
-        print(f"The error is {error}")
+        print(f"Error while connecting to PostgreSQ {error}")
+
     return connection
 
 
+def create_products():
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(""" CREATE TABLE IF NOT EXISTS products(
+            ID SERIAL PRIMARY KEY,
+            NAME TEXT);
+            """)
+
+
+def create_flows():
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(""" CREATE TABLE IF NOT EXISTS flows(
+            id SERIAL PRIMARY KEY,
+            name TEXT);
+            """)
+
+
+def create_countries():
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(""" CREATE TABLE IF NOT EXISTS countries(
+            id SERIAL PRIMARY KEY,
+            name TEXT,
+            latitude TEXT,
+            longitude TEXT);
+            """)
+
+
+def create_quantities():
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(""" CREATE TABLE IF NOT EXISTS quantities(
+            id SERIAL PRIMARY KEY,
+            year INT,
+            value INT,
+            product_id INT REFERENCES products(id),
+            country_id INT REFERENCES countries(id),
+            flow_id INT REFERENCES flows(id));
+            """)
+
+
 connection = create_conection(user, password, host, port, name)
+
+create_products()
+create_flows()
+create_countries()
+create_quantities()
+
+uri = f'postgres://{user}:{password}@{host}:{port}/{name}'
+engine = create_engine(uri)
+df2.to_sql('countries', con=engine, if_exists='append', index=False)
+engine.execute("SELECT * FROM countries").fetchall()
