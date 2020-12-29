6c3baae790c3d7d98dcd4fd07a2303eb25232f3a
Auore List <asunami.al@gmail.com>
2020-12-29 16:25:37 +0100

    création insert quantities

--- insert.py
+++ insert.py
@@ -3,14 +3,12 @@ from sqlalchemy import create_engine
 from database_request import insert_quantities
 from config import *
 
-
 # Connection à la db
 uri = f'postgres://{user}:{password}@{host}:{port}/{name}'
 engine = create_engine(uri)
 # Pour mettre toute les donnée du df dans la db
 products.to_sql(name="products", con=engine, if_exists="append", index=False)
 flows.to_sql(name="flows", con=engine, if_exists="append", index=False)
-
 
 # partie clement
 df2.to_sql('countries', con=engine, if_exists='append', index=False)
